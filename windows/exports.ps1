8820b209c9aab273dfffaa45e7653464b3c1706f
George <28628499+largerock@users.noreply.github.com>
2024-09-25 13:39:38 +1000

    add default path export to profile for vscode and terminal

--- windows/exports.ps1
+++ windows/exports.ps1
@@ -2,3 +2,5 @@
 Set-Environment "EDITOR" "nano"
 Set-Environment "GIT_EDITOR" $Env:EDITOR
 Set-Environment "SSH_GIT" "C:\Windows\System32\OpenSSH\ssh.exe"
+
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
